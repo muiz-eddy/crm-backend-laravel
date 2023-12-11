19d8f2129963007ef4da248361a79687fb24b3ae
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2023-12-11 20:10:13 +0800

    Added: New Migration for table games

---
+++ app/Models/Steam_Games.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Steam_Games extends Model
+{
+    use HasFactory;
+}
