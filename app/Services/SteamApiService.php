3a0cdde753ca296b374f51ff6251276592a9dbb7
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2023-12-18 19:30:39 +0800

    Modify: made skeleton for the code

--- app/Services/SteamApiService.php
+++ app/Services/SteamApiService.php
@@ -1,24 +1,39 @@
 <?php
+
 namespace App\Services;
 
+use Illuminate\Console\Command;
 use GuzzleHttp\Client;
+use App\Enums\SteamApiUrl;
+use App\Models\Steam_Games;
 
-class SteamApiService {
-
+class SteamApiService extends Command
+{
     protected $client;
+    protected $signature = 'test:steamapi';
 
-    public function __construct(Client $client) {
-        $this ->client = $client;
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
     }
 
-    public function fetchData() {
+    public function fetchData()
+    {
         try {
             //TODO: Implement url using enum
-            $response = $this->client->request('GET', 'url');
-        }
-        catch(\Exception $e) {
+            $response = $this->client->request('GET', SteamApiUrl::API_STEAM_BASE_URL . SteamApiUrl::GET_STEAM_ID);
 
+            //Json Decode
+            $data = json_decode($response->getBody(), true);
+            dd($data);
+        } catch (\Exception $e) {
         }
     }
 
+    protected function saveData($data)
+    {
+        Steam_Games::create(
+            []
+        );
+    }
 }
