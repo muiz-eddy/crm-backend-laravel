19313642c95f069805bc6154d179352d1086662f
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2023-12-19 20:08:19 +0800

    Modify: formatting

--- app/Enums/SteamApiUrl.php
+++ app/Enums/SteamApiUrl.php
@@ -5,6 +5,5 @@ namespace App\Enums;
 enum SteamApiUrl: string
 {
     case API_STEAM_BASE_URL = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
-    case STORE_STEAM_BASE_URL = 'https://store.steampowered.com/';
-    case APP_DETAILS = 'api/appdetails?appids=';
+    case STORE_STEAM_BASE_URL = 'https://store.steampowered.com/api/appdetails?appids=';
 }
