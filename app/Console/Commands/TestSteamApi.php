cf2b502da4379012a8b47162da341cc522494480
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2023-12-19 20:07:40 +0800

    Modify: Done with testing

--- app/Console/Commands/TestSteamApi.php
+++ app/Console/Commands/TestSteamApi.php
@@ -32,19 +32,28 @@ class TestSteamApi extends Command
             $client = new Client();
             //TODO: Implement url using enum
             $response = $client->request('GET', SteamApiUrl::API_STEAM_BASE_URL->value);
-
+            $responseSteam = '';
             //Json Decode
             $data = json_decode($response->getBody(), true);
 
             if (isset($data['applist']['apps'])  && is_array($data['applist']['apps'])) {
+                $requestCount = 0;
                 foreach ($data['applist']['apps'] as $app) {
                     if (isset($app['appid'])) {
-                        print_r($app['appid']);
+                        $responseSteam = $client->request('GET', SteamApiUrl::STORE_STEAM_BASE_URL->value . $app['appid']);
+                        $data2 = json_decode($responseSteam->getBody(), true);
+                        print_r($data2);
                         echo PHP_EOL;
+                        sleep(1.5);
+
+                        if ($requestCount >= 200) {
+                            break;
+                        }
                     }
                 }
             }
         } catch (\Exception $e) {
+            print_r($e);
         }
     }
 }
